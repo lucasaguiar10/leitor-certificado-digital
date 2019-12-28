f9ab215a5b2e73f01f61988cac5034b9357fea31
thiagopetherson <44420212+thiagopetherson@users.noreply.github.com>
2019-12-28 16:43:41 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function()
+{
+
+
+	$('#formulario-certificado-digital').submit(function(e)
+	{			
+		e.preventDefault();
+		var formulario = $(this);
+		let retorno_transferir = salvarCertificado();	
+		
+	});
+
+
+
+	function salvarCertificado()
+	{	
+
+		let formulario = $('#formulario-certificado-digital')[0];
+		let data = new FormData(formulario);
+
+		$.ajax
+		({		
+			url:"php/pagina-certificado/salvar-certificado.php",		
+			type:"POST",
+			data: data,
+			processData: false,
+			contentType: false	
+			
+		}).done(function(data)
+		{	
+			console.log(data);			
+			alert(data.razaosocial);
+			alert(data.cnpj);
+			alert(data.validade);
+		
+			$('#senha-certificado').val("");
+			$('#certificado-digital').val("");
+		
+		}).fail(function()
+		{
+			
+		}).always(function()
+		{
+
+		});		
+		
+	}
+
+
+
+});
+	
